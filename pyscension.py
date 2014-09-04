e0afd9a1c8b6144ca81b00e5918ff90cc5c11331
Chrijj <chrijj@gmail.com>
2014-09-04 22:31:42 +0100

    superclass

--- pyscension.py
+++ pyscension.py
@@ -12,33 +12,33 @@ import csv
 # hence the issue that was being caused by hands full of letters
 
 # cardName: [runes generated, power generated, honour, cost]
+# this will be populated from the csv file
 card_list = {"Apprentice":[1, 0, 0, 0], "Militia":[0,1,0,0]}
+
+# need a print value for cards to make information clearer [Apprentice // 1 Rune // 0 Honour] [ MONSTER: Name // x Power // y Honour // z Special Effects]
 
 def handInstructions():
 	"""tells the player what to do"""
 	print "To play a card, input the number next to the card."
 	print "Enter 'END' to end your turn"
 
-
-class player(object):
-	"""each individual player"""
+class deck(object):
+	"""This is a base used for player decks and the game 'deck'"""
+	# game hand = board, discard = banish (used by players also), power = remaining resources etc.
 	def __init__(self, name):
 		self.name = name
 		self.deck = []
-		self.newDeck()
-		self.discard = []
 		self.hand = []
+		self.discard = []
+
+
+class player(deck):
+	"""each individual player"""
+	def __init__(self, name):
+		super(player, self).__init__(name)
 		self.honour = 0
+		self.newDeck() # should these be moved to their own method with the parent class now taking precedence?
 		self.newHand()
-
-		"""print self.name
-		print "----"
-		for card in self.deck:
-			print card
-		print "hand:", self.hand"""
-
-		self.displayHand()
-
 
 	def newDeck(self):
 		"""player crafted deck - starts with:
@@ -53,7 +53,7 @@ class player(object):
 		self.discard = []
 
 	def drawCard(self):
-		# do i need to account for the incredibly unlikely event of a discard and deck having no cards in them
+		# do i need to account for the incredibly unlikely event of discard and deck having no cards in them?
 		if len(self.deck) == 0:
 			self.deck = list(self.discard)
 			self.discard = []
@@ -62,20 +62,24 @@ class player(object):
 		self.hand.append(self.deck.pop()) # assume this pops the 0'th indexed item by default but havet to check
 
 	def newHand(self):
+		#this could be simplified to discard hand // new hand to have a single unified parent method that just draws to five - would work for the game state and the player
 		if len(self.hand) != 0:
 			for card in self.hand:
 				self.discard.append(card)
-				self.hand = [] # would be cleaner using pop but for lists that requires iterating over the indexes
-		for i in range(5):
+				self.hand = [] # would be cleaner using pop but for lists that requires iterating over the indexes - why does this mean I can't use pop?
+		for i in range(5):	# any value to a global variable for cards in hand?
 			self.drawCard()
 		
 	def displayHand(self):
 		# print "%s hand:\n %s" % (self.name, [(i, card) for card in self.hand for i in range(5)]) # this does not provide the output I desire
 		print "%s's hand:" % self.name
 		for x in range(len(self.hand)):
-			print x,":", self.hand[x] 
+			print x,":", self.hand[x] # intention is for hand to be interacted with via numbers whilst the board/game is via letters (iterate over a list ['q','w','e', etc])
+
 
 	def playHand(self):
+		# this will need simplifying so that it just forms part of a whole related to a game tuen
+		# runes / power perhaps becoming part of the self that are then cleared at the end of the turn to facilitate this
 		handInstructions()
 		cardSel = ""
 		runes = 0
@@ -116,10 +120,14 @@ class player(object):
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-# x = player("Steve")
-# x.playHand()
+x = player("Steve")
 
-class board(object):
+print x.deck
+#x.newDeck()
+#x.newHand()
+x.playHand()
+
+"""class board(object):
 	def __init__(self):
 		self.deck = []
 		self.constants = {}
@@ -127,7 +135,7 @@ class board(object):
 
 
 class monsterDeck(object):
-	"""Monster / Central deck"""
+	""Monster / Central deck"
 	def __init__(self):
 		self.deck = []
 		self.discard = []
@@ -139,8 +147,4 @@ with open('CotG.csv', 'rb') as f:
         print row[1]
 
 # probably best to front load this information into memory when first run
-
-
-
-
-
+"""
